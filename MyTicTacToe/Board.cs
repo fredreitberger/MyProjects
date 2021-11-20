19ca7dbbb48dad213916095f04c6f0a76a1737ca
F <fcr@b.com>
2021-11-20 10:18:53 -0500

    Add project files.

---
+++ MyTicTacToe/Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTicTacToe
+{
+    class Board
+    {
+        /// <summary>
+        /// Tokens that can be placed on the board
+        /// </summary>
+        enum BoardToken
+        {
+            BT_Empty,
+            BT_X,
+            BT_O
+        }
+
+        /// <summary>
+        ///  The board is an array of tokens, 9 deep, arranged as a 3x3 grid
+        /// </summary>
+        private BoardToken[] grid;
+
+        
+    }
+}
---
+++ MyTicTacToe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string input = Console.ReadLine();
+            int a = Int32.Parse(input);
+
+            Console.WriteLine("You wrote {0}", a);
+            Console.ReadKey();
+        }
+    }
+}
