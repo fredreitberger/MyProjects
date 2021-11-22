b09eaeeb00541b66f2558463e8b2113d0fd58f38
F <fcr@b.com>
2021-11-22 10:55:07 -0500

    Adding deck functionality and ability to shuffle a pile of cards

--- War/CardPile.cs
+++ War/CardPile.cs
@@ -13,5 +13,39 @@ namespace War
     {
 
         private Card[] pile;
+
+
+        /// <summary>
+        /// Returns a new standard 52-card deck
+        /// </summary>
+        /// <returns></returns>
+        public static CardPile StandardDeck()
+        {
+            CardPile newPile = new CardPile();
+
+            foreach (Card.CardRank rank in Enum.GetValues(typeof(Card.CardRank)))
+            {
+                newPile.pile.Append(new Card(rank, Card.SUIT_HEARTS));
+                newPile.pile.Append(new Card(rank, Card.SUIT_CLUBS));
+                newPile.pile.Append(new Card(rank, Card.SUIT_DIAMONDS));
+                newPile.pile.Append(new Card(rank, Card.SUIT_SPADES));
+            }
+
+            return newPile;
+        }
+
+
+        public void Shuffle()
+        {
+            Random rng = new Random();
+
+            for (int i = 0; i < pile.Length; i++)
+            {
+                Card tmp = pile[i];
+                int swap = rng.Next(0, pile.Length);
+                pile[i] = pile[swap];
+                pile[swap] = tmp;
+            }
+        }
     }
 }
