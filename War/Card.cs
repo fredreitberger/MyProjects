b083d6db7e1307ac36a24b8f5643b21af5d09612
F <fcr@b.com>
2021-11-22 10:13:35 -0500

    Adding project for the War card game

---
+++ War/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace War
+{
+    class Card
+    {
+        private readonly byte rank;
+
+        private readonly char suit;
+
+        private const char SUIT_HEARTS   = 'H';
+        private const char SUIT_CLUBS    = 'C';
+        private const char SUIT_SPADES   = 'S';
+        private const char SUIT_DIAMONDS = 'D';
+
+
+
+    }
+}
